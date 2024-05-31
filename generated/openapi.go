bd37e33a54e69f516f07481800ed16f2a6b9dbf5
patrik-rangel <patrik.souza@sptech.school>
2024-05-31 19:07:05 -0300

    code for generate

--- generated/openapi.go
+++ generated/openapi.go
@@ -5,3 +5,6 @@ import _ "embed"
 //go:generate go run main.go -generate ../api/api_docs.yml openapi/api_docs_specs.yml
 //go:embed openapi/api_docs_specs.yml
 var APIDocsSpecs []byte
+
+// else
+// go run main.go -generate ../api/api_docs.yml openapi/api_docs_specs.yml
