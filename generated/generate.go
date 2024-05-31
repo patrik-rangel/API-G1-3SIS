ec3c18365244f286664dd20289bad844feb16dfd
patrik-rangel <patrik.souza@sptech.school>
2024-05-31 17:10:41 -0300

    create generated code

---
+++ generated/generate.go
@@ -0,0 +1,3 @@
+package generated
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --package openapi --target ./openapi --clean ../api/api_docs.yml
---
+++ generated/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	source := flag.String("generate", "", "Path to the source API Docs YAML file")
+	flag.Parse()
+
+	if *source == "" {
+		fmt.Println("Missing source file path. Use the -generate flag.")
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadFile(*source)
+	if err != nil {
+		fmt.Println("Error reading source file:", err)
+		os.Exit(1)
+	}
+
+	err = ioutil.WriteFile("openapi/api_docs_specs.yml", data, 0644)
+	if err != nil {
+		fmt.Println("Error writing to destination file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("API Docs YAML file copied successfully!")
+}
