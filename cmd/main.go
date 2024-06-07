b06916a5e52dc2555fa709500ac11ef1665eac1f
patrik-rangel <patrik.souza@sptech.school>
2024-06-07 17:57:38 -0300

    connect database

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	"github.com/klauspost/compress/gzhttp"
+
 	"github.com/patrik-rangel/API-G1-3SIS/generated/openapi"
 	"github.com/patrik-rangel/API-G1-3SIS/internal/api"
+	"github.com/patrik-rangel/API-G1-3SIS/internal/domain/service"
+	"github.com/patrik-rangel/API-G1-3SIS/internal/resource/database"
 )
 
 func main() {
@@ -16,7 +22,13 @@ func main() {
 		panic("fail to load the .env")
 	}
 
-	handler := api.NewHandler()
+	db := connDb()
+
+	userGtw := database.New(db)
+
+	userService := service.NewUser(userGtw)
+
+	handler := api.NewHandler(userService)
 	security := api.NewMySecurityHandler(os.Getenv("AUTHORIZATION_SERVICE"))
 	server, err := openapi.NewServer(handler, security)
 	if err != nil {
@@ -24,4 +36,13 @@ func main() {
 	}
 
 	http.ListenAndServe(":8080", gzhttp.GzipHandler(server))
+}
+
+func connDb() *sqlx.DB {
+	db, err := sqlx.Connect("pgx", "user=grupo1 password=grupo1 host=localhost port=5432 database=grupo1 sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+		panic("error in connection database")
+	}
+	return db
 }
