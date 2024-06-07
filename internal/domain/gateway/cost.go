b43868743cda6288ea5cc069463d6c09f62132e1
patrik-rangel <patrik.souza@sptech.school>
2024-06-07 17:57:18 -0300

    create service and gateway for entity cost

---
+++ internal/domain/gateway/cost.go
@@ -0,0 +1,15 @@
+package gateway
+
+import (
+	"context"
+	"github.com/patrik-rangel/API-G1-3SIS/internal/domain/entity"
+)
+
+type Cost interface {
+	InsertVariableExpense(ctx context.Context, variableExepense entity.VariableExepense) error
+	UpdateVariableExepense(ctx context.Context, id int, variableExepense entity.VariableExepense) error
+	InsertCostCenter(ctx context.Context, costCenter entity.CostCenter) error
+	GetVariableExpensesByCostCenter(ctx context.Context, id int) ([]*entity.VariableExepense, error)
+	GetCostCenterById(ctx context.Context, id int) (*entity.CostCenter, error)
+	GetEmployeesByCostCenter(ctx context.Context, id int) ([]*entity.VariableExepense, error)
+}
