f07c1120579e4c8aa321062bf8b11b951feb9e46
patrik-rangel <patrik.souza@sptech.school>
2024-06-07 17:55:18 -0300

    implements handler

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,19 +1,44 @@
 package api
 
 import (
+	"context"
+	"errors"
+
 	logger "github.com/patrik-rangel/API-G1-3SIS/config/log"
 	"github.com/patrik-rangel/API-G1-3SIS/generated/openapi"
+	"github.com/patrik-rangel/API-G1-3SIS/internal/domain/entity"
+	"github.com/patrik-rangel/API-G1-3SIS/internal/domain/service"
 	"go.uber.org/zap"
 )
 
 var _ openapi.Handler = (*Handler)(nil)
 
 type Handler struct {
-	log *zap.Logger
+	log         *zap.Logger
+	userService service.User
 }
 
-func NewHandler() *Handler {
+func NewHandler(c service.User) *Handler {
 	return &Handler{
-		log: logger.NewLogger().Named("layer-handler"),
+		log:         logger.NewLogger().Named("layer-handler"),
+		userService: c,
+	}
+}
+
+func (h *Handler) NewError(ctx context.Context, err error) *openapi.ErrorResponseStatusCode {
+	statusCode := 500
+	msg := entity.ErrGeneric.Error()
+
+	switch {
+	case errors.Is(err, entity.ErrNotFoundUser):
+		statusCode = 401
+		msg = entity.ErrNotFoundUser.Error()
+	}
+
+	return &openapi.ErrorResponseStatusCode{
+		StatusCode: statusCode,
+		Response: openapi.ErrorResponse{
+			Message: openapi.NewOptString(msg),
+		},
 	}
 }
