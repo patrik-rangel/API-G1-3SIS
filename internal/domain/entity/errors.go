66800f1acebc79a095af3d604e8dbd6b40c55459
patrik-rangel <patrik.souza@sptech.school>
2024-06-08 19:51:58 -0300

    create new errors

--- internal/domain/entity/errors.go
+++ internal/domain/entity/errors.go
@@ -5,3 +5,15 @@ import "errors"
 var ErrNotFoundUser = errors.New("NOT_FOUND_USER")
 
 var ErrGeneric = errors.New("GENERIC_ERROR")
+
+var ErrDatabase = errors.New("DATABASE_ERROR")
+
+var ErrUserExists = errors.New("USER_EXISTS")
+
+var ErrExecutiveExists = errors.New("EXECUTIVE_EXISTS")
+
+var ErrCostCenterExists = errors.New("COST_CENTER_EXISTS")
+
+var ErrVariableExpenseInvalid = errors.New("INVALID_VARIABLE_EXPENSE")
+
+var ErrNotFoundCostCenter = errors.New("NOT_FOUND_COST_CENTER")
